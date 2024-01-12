daa25a12f9ba812ded66fc2bd2846b0ae6b8a1a7
Francisco José Ruiz Vera <francisco@regionalseguros.com.py>
2024-01-12 10:52:51 -0300

    Updated MyLibrary.py for push test

--- libraries/MyLibrary.py
+++ libraries/MyLibrary.py
@@ -15,3 +15,13 @@ class MyLibrary:
     def click_print(self):
         locator = "image:resources/print_button.png"
         self.desktop.click(locator)
+
+    
+    @keyword
+    def extraer_capital(self, texto):
+        inicio = texto.find("xima del Articulo:") + 18
+        fin = texto.find("Prima del Articulo")
+        texto = texto[inicio:fin].strip("\n")
+        texto = texto.replace(".","").replace(",",".")
+
+        return int(texto)
